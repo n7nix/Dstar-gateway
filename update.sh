f801d221b4a9fc9f0b17f795aa9678aa912edbd7
n7nix <basil@pacabunga.com>
2021-08-20 14:45:11 -0700

    Restart systemd services after a file update.

--- update.sh
+++ update.sh
@@ -7,7 +7,11 @@
 cp atom_restart.sh /usr/local/bin
 cp atom_connection.service /etc/systemd/system
 cp atom_connection.timer /etc/systemd/system
+
 systemctl daemon-reload
+systemctl restart atom_connection.service
+systemctl restart atom_connection.timer
+echo
 echo "=== status"
 echo
 systemctl --no-pager status "atom_connection.*"
