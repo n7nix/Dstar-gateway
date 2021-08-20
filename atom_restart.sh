0eaf0ecb3a7c93f2915df836fa35acd324c56083
n7nix <basil@pacabunga.com>
2021-08-20 14:12:20 -0700

    Remove DEBUG logmsgs

--- atom_restart.sh
+++ atom_restart.sh
@@ -308,14 +308,14 @@ fi
 
 # Get PID of parent process
 PPPID=$(ps h -o ppid= $PPID)
-logmsg "DEBUG: PPPID= $PPPID, PPID= $PPID"
+# logmsg "DEBUG: PPPID= $PPPID, PPID= $PPID"
 
 if (( $PPPID == 0 )) ; then
     PPPID=1
 fi
 # get name of parent process
 P_COMMAND=$(ps h -o %c $PPPID)
-logmsg "DEBUG: P_COMMAND=$PCOMMAND"
+# logmsg "DEBUG: P_COMMAND=$PCOMMAND"
 
 # Check if local log directory exists.
 # Use this for debugging
